9bb611b87dc548d99695ad5bdbc5bf5820a13c08
Nimgoble <nimgoble@gmail.com>
2020-07-22 12:56:31 -0600

    Fetch Quest Section 2 Objective 1
    "Find the chest" now works.

--- Aerandyr/Assets/Scripts/GTwinChest.cs
+++ Aerandyr/Assets/Scripts/GTwinChest.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 public class GTwinChest : Chest
 {
 	private Quest ourQuest = null;
 	private QuestSection ourQuestSection = null;
+	private ColliderCallbackSource colliderCallbackSource;
 	protected override void Start()
 	{
 		base.Start();
 		ourQuest = QuestManager.Instance.GetQuest("Test Quest");
 		if(ourQuest != null)
 			ourQuestSection = ourQuest.Sections[1];
+		colliderCallbackSource = GetComponentInChildren<ColliderCallbackSource>();
+		if(colliderCallbackSource != null)
+		{
+			colliderCallbackSource.TriggerEnter += this.OnSeenChest;
+		}
+	}
+	protected void OnSeenChest(GameObject sender, Collider2D ourCollider, Collider2D other)
+	{
+		if (ourQuestSection == null || ourQuestSection.Objectives[0].IsDone || ourQuest.CurrentSection != ourQuestSection)
+			return;
+		ourQuestSection.Objectives[0].IsDone = true;
+		colliderCallbackSource.TriggerEnter -= this.OnSeenChest;
 	}
 	protected override bool IsLocked { get { return ourQuest == null ? true : ourQuest.CurrentSection != ourQuestSection; } }
 	protected override void ChestOpened()
